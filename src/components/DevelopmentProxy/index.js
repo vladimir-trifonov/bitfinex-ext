31f7aeeeee2590e08c2e4d40db469566467f4c1b
vladimir-trifonov <trifonov.dev@gmail.com>
2018-12-04 20:02:43 +0200

    Added development proxy

---
+++ src/components/DevelopmentProxy/index.js
@@ -0,0 +1,20 @@
+export default function () {
+  if (process.env.NODE_ENV !== 'development') return
+
+  var cors_api_host = process.env.REACT_APP_BITFINEX_API_URL;
+  var cors_api_url = 'https://' + cors_api_host + '/';
+  var slice = [].slice;
+  var origin = window.location.protocol + '//' + window.location.host;
+  var open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function() {
+      var args = slice.call(arguments);
+      var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+      if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+          targetOrigin[1] !== cors_api_host) {
+          args[1] = cors_api_url + args[1];
+      }
+      return open.apply(this, args);
+  }
+
+  return null
+}
